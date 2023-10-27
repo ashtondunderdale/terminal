9d2af98af830cbfebd10fd45c3da508ab14a3bdd
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-10-27 17:03:04 +0100

    added time and clear commands + command descriptions

--- sharpTerminal/Commands.cs
+++ sharpTerminal/Commands.cs
@@ -1,10 +1,17 @@
-﻿using System;
-
-
-namespace sharpTerminal
+﻿namespace sharpTerminal
 {
     class Commands
     {
+        private Dictionary<string, Action> CommandMap;
+        private Dictionary<string, string> CommandDescription;
+
+
+        public void SetCommandMap(Dictionary<string, Action> commandMap, Dictionary<string, string> commandDescription)
+        {
+            CommandMap = commandMap;
+            CommandDescription = commandDescription;
+        }
+
         public void HelloWorld()
         {
             Console.WriteLine("Hello World!\n");
@@ -14,5 +21,29 @@ namespace sharpTerminal
         {
             Program.Active = false;
         }
+
+        public void Help()
+        {
+            Console.WriteLine("List of Commands:\n");
+
+            foreach (var command in CommandMap.Keys)
+            {
+                Console.WriteLine($"{command.PadRight(10)} - {CommandDescription[command]}");
+            }
+            Console.WriteLine();
+        }
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        public void Time()
+        {
+            Console.WriteLine(DateTime.UtcNow);
+        }
     }
 }
+
+
+
+
--- sharpTerminal/Program.cs
+++ sharpTerminal/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 
-namespace sharpTerminal 
+namespace sharpTerminal
 {
     class Program
     {
@@ -10,12 +10,25 @@ namespace sharpTerminal
         static void Main(string[] args)
         {
             var commands = new Commands();
-
             var commandMap = new Dictionary<string, Action>
             {
                 { "hello", commands.HelloWorld },
                 { "exit", commands.Exit },
+                { "help", commands.Help },
+                { "clear", commands.Clear },
+                { "time", commands.Time },
             };
+
+            var commandDescriptions = new Dictionary<string, string>
+            {
+                { "hello", "Greets the user" },
+                { "exit", "Exits the program" },
+                { "help", "Displays a list of available commands" },
+                { "clear", "Clears the console" },
+                { "time", "Displays the current time (UTC)" },
+            };
+
+            commands.SetCommandMap(commandMap, commandDescriptions);
 
             while (Active)
             {
@@ -26,21 +39,17 @@ namespace sharpTerminal
                 {
                     commandMap[input]();
                 }
-
                 else if (string.IsNullOrEmpty(input))
-
                 {
                     continue;
                 }
-
                 else
                 {
-                    Console.WriteLine($"'{input}' is not recognised as a command.\n");
+                    Console.WriteLine($"'{input}' is not recognized as a command.\n");
                 }
             }
         }
     }
 }
-
 
 
