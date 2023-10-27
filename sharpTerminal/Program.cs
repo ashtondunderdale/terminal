255b436ebf03ec17376091801cbe729377dd2924
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-10-27 13:05:52 +0100

    Add project files.

---
+++ sharpTerminal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var commands = new Commands();
+        var commandMap = new Dictionary<string, Action>
+        {
+            { "hello", commands.Hello },
+        };
+
+        while (true)
+        {
+            Console.Write("Command: ");
+            string input = Console.ReadLine().ToLower(); 
+
+            if (input == "exit")
+            {
+                break;
+            }
+
+            if (commandMap.ContainsKey(input))
+            {
+                commandMap[input]();
+            }
+            else if (input == "")
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine($"'{input}' is not recognised as a command.\n");
+            }
+        }
+    }
+}
+
+class Commands
+{
+    public void Hello()
+    {
+        Console.WriteLine("Hello World!\n");
+    }
+}
